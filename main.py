d240c1325b585c2b651fa8b5d7bcd273434266ec
EmmettDoguwu <emmettbicker@hunterschools.org>
2020-11-17 19:42:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+num1 = ("Hello my name is Emot")
+num2 = (" Hello my name is")
+print(num1 + num2)
+
+num1 = ("Hello my name is Emot")
+num2 = ("Hello my name is")
+print(num1, num2)
+
+
+print("Hello, this is the government. Enter how much you think your medical bill should cost")
+num1 = int(input("Enter a number, then press enter: "))
+print("Thank you, now please enter how many seconds your appointment took")
+num2 = int(input("Enter another number, then press enter: "))
+print("The sum is", num1 ** num2)
+print("Thank you, here is your medical bill")
